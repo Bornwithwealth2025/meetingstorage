cb9f378f6417c5c851a0dc5ad15a3dbb91cfd81a
Bornwithwealth2025 <info@bornwithwealth.com>
2026-01-19 09:54:46 +0000

    recording done set allow bww

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const PORT = process.env.PORT || 4000;
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL_LOCAL || '*',
-  credentials: true
+  //origin: process.env.CLIENT_URL_LOCAL || '*',
+  origin: process.env.CLIENT_URL,
+  credentials: tru//e
 }));
 
 app.use(express.json({ limit: '100mb' }));
@@ -26,7 +27,8 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || '*',
+   // origin: process.env.CLIENT_URL || '*',
+     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST']
   },
   transports: ['websocket', 'polling'],
